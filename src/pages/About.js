f0434c26c7fe8853201b3a3455285e84e1fd0ba9
daniel ung <daniel.ung@sjsu.edu>
2024-06-09 22:32:11 -0700

    Implemented Home, About, and AddToDo

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-  
+import profile_pic from '../assets/profile_pic.jpeg';
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img
+                className="profile_image"
+                src={profile_pic}
+                alt="Profile Pic"
+              ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Daniel Ung</div>
+              <div className="brief_description">
+                  Incoming Software Engineer intern at SAP + Chess entiusiast
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
